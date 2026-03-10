8e97a539c39c9c1cdf5357f08e4c750385b5bc0f
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-03-10 13:04:30 +0800

    chore: add FastCSV

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation(kotlin("stdlib-jdk8"))
+    implementation("de.siegmar:fastcsv:4.1.1")
 }
 
 tasks.test {
