9a3e2ce3923fa694717947156b5beaecc72eda91
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-03-20 10:38:07 +0800

    chore: cleanup imports

--- src/main/java/org/motorph/Main.java
+++ src/main/java/org/motorph/Main.java
@@ -1,24 +1,6 @@
 package org.motorph;
 
-import de.siegmar.fastcsv.reader.CsvReader;
-import org.motorph.employees.Employee;
-import org.motorph.employees.EmploymentStatus;
-import org.motorph.employees.Login;
 import org.motorph.payroll.ConsolePayroll;
-import org.motorph.payroll.PayrollService;
-import org.motorph.timesheet.Timesheet;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.text.NumberFormat;
-import java.text.ParseException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
