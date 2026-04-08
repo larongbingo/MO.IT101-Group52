d0e6d238839a2c49d51f5502bae0574859ec7998
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-04-09 00:38:29 +0800

    refactor: move each prop into a single line

--- motorph-payroll-core/src/org/motorph/employees/dto/NewEmployeeDto.java
+++ motorph-payroll-core/src/org/motorph/employees/dto/NewEmployeeDto.java
@@ -1,6 +1,18 @@
 package org.motorph.employees.dto;
 
-public record NewEmployeeDto(String employeeId, String lastName, String firstName, String birthday, String address,
-                             String phoneNumber, String sssNumber, String philHealthNumber, String taxIdNumber,
-                             String pagibigMemberIdNumber, String employmentStatus, String position,
-                             String basicSalary) {}
+public record NewEmployeeDto(
+        String employeeId,
+        String lastName,
+        String firstName,
+        String birthday,
+        String address,
+        String phoneNumber,
+        String sssNumber,
+        String philHealthNumber,
+        String taxIdNumber,
+        String pagibigMemberIdNumber,
+        String employmentStatus,
+        String position,
+        String basicSalary,
+        String supervisorId
+) { }
--- motorph-payroll-core/src/org/motorph/employees/dto/UpdateEmployeeDto.java
+++ motorph-payroll-core/src/org/motorph/employees/dto/UpdateEmployeeDto.java
@@ -1,6 +1,17 @@
 package org.motorph.employees.dto;
 
-public record UpdateEmployeeDto(String employeeId, String lastName, String firstName, String birthday, String address,
-                                String phoneNumber, String sssNumber, String philHealthNumber, String taxIdNumber,
-                                String pagibigMemberIdNumber, String employmentStatus, String position,
-                                String basicSalary, String SupervisorId) {}
+public record UpdateEmployeeDto(
+        String LastName,
+        String FirstName,
+        String Birthday,
+        String Address,
+        String PhoneNumber,
+        String SSSNumber,
+        String PhilHealthNumber,
+        String TaxIdNumber,
+        String PagibigMemberIdNumber,
+        String EmploymentStatus,
+        String Position,
+        String BasicSalary,
+        String SupervisorId
+) {}
