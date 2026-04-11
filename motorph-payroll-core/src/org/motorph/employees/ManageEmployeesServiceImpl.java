e67b29ae9cc9933ca95a3bc0b14059adaf97604b
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-04-12 01:51:03 +0800

    refactor: move employee creation logic

--- motorph-payroll-core/src/org/motorph/employees/ManageEmployeesServiceImpl.java
+++ motorph-payroll-core/src/org/motorph/employees/ManageEmployeesServiceImpl.java
@@ -24,11 +24,6 @@ public final class ManageEmployeesServiceImpl implements ManageEmployeesService 
             return Result.failure(new MotorPhException("DTO is null"));
         }
 
-        var employmentStatus = EmploymentStatus.parse(newEmployee.employmentStatus());
-        if (employmentStatus instanceof Failure<EmploymentStatus>(MotorPhException exception)) {
-            return Result.failure(exception);
-        }
-
         var supervisor = employeeRepository.getEmployeeByEmployeeId(newEmployee.supervisorId());
         if (supervisor == null) {
             return Result.failure(new MotorPhException(newEmployee.supervisorId() + " is not a valid ID"));
@@ -39,38 +34,12 @@ public final class ManageEmployeesServiceImpl implements ManageEmployeesService 
             return Result.failure(new MotorPhException("ID is already taken"));
         }
 
-        try {
-            var employee = new Employee(
-                    newEmployee.employeeId(),
-                    newEmployee.lastName(),
-                    newEmployee.lastName(),
-                    LocalDate.parse(newEmployee.birthday(), DateTimeFormatter.ofPattern("MM/dd/yyyy")),
-                    newEmployee.address(),
-                    newEmployee.phoneNumber(),
-                    newEmployee.sssNumber(),
-                    newEmployee.philHealthNumber(),
-                    newEmployee.taxIdNumber(),
-                    newEmployee.pagibigMemberIdNumber(),
-                    ((Success<EmploymentStatus>)employmentStatus).value(),
-                    newEmployee.position(),
-                    Double.parseDouble(newEmployee.basicSalary())
-            );
-            employee.SupervisorId = supervisor.EmployeeId;
-
-            var insertResult = employeeRepository.addEmployee(employee);
-            if (insertResult instanceof Failure<Employee>(MotorPhException exception)) {
-                return Result.failure(
-                        new MotorPhException("Failed to add the employee into database", exception));
-            }
-
-            return Result.success(employee);
-        } catch (NullPointerException e) {
-            return Result.failure(new MotorPhException("Null value found on one of the fields", e));
-        } catch (DateTimeParseException e) {
-            return Result.failure(new MotorPhException("Birthday can't be parsed into date", e));
-        } catch (NumberFormatException e) {
-            return Result.failure(new MotorPhException("Basic Salary can't be parsed into double", e));
+        var employeeResult = newEmployee.toEmployee();
+        if (employeeResult instanceof Failure<Employee>(MotorPhException exception)) {
+            return Result.failure(exception);
         }
+
+        return employeeRepository.addEmployee(((Success<Employee>)employeeResult).value());
     }
 
     public Result<Employee> updateEmployee(Employee employeeToUpdate, UpdateEmployeeDto updateEmployee) {
--- motorph-payroll-core/src/org/motorph/employees/dto/NewEmployeeDto.java
+++ motorph-payroll-core/src/org/motorph/employees/dto/NewEmployeeDto.java
@@ -1,5 +1,16 @@
 package org.motorph.employees.dto;
 
+import org.motorph.core.MotorPhException;
+import org.motorph.core.results.Failure;
+import org.motorph.core.results.Result;
+import org.motorph.core.results.Success;
+import org.motorph.employees.Employee;
+import org.motorph.employees.EmploymentStatus;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public record NewEmployeeDto(
         String employeeId,
         String lastName,
@@ -15,4 +26,38 @@ public record NewEmployeeDto(
         String position,
         String basicSalary,
         String supervisorId
-) { }
+) {
+    public Result<Employee> toEmployee() {
+        var employmentStatus = EmploymentStatus.parse(this.employmentStatus());
+        if (employmentStatus instanceof Failure<EmploymentStatus>(MotorPhException exception)) {
+            return Result.failure(exception);
+        }
+
+        try {
+            var employee = new Employee(
+                    this.employeeId(),
+                    this.lastName(),
+                    this.lastName(),
+                    LocalDate.parse(this.birthday(), DateTimeFormatter.ofPattern("MM/dd/yyyy")),
+                    this.address(),
+                    this.phoneNumber(),
+                    this.sssNumber(),
+                    this.philHealthNumber(),
+                    this.taxIdNumber(),
+                    this.pagibigMemberIdNumber(),
+                    ((Success<EmploymentStatus>)employmentStatus).value(),
+                    this.position(),
+                    Double.parseDouble(this.basicSalary())
+            );
+
+            return Result.success(employee);
+        }
+        catch (NullPointerException e) {
+            return Result.failure(new MotorPhException("Null value found on one of the fields", e));
+        } catch (DateTimeParseException e) {
+            return Result.failure(new MotorPhException("Birthday can't be parsed into date", e));
+        } catch (NumberFormatException e) {
+            return Result.failure(new MotorPhException("Basic Salary can't be parsed into double", e));
+        }
+    }
+}
