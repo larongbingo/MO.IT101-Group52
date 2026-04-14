e712557d2b16ffa0470481b13fcc5caed4a28143
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-04-14 23:15:47 +0800

    feat: init Koin DI

--- motorph-payroll-app/jvm-app/src/main.kt
+++ motorph-payroll-app/jvm-app/src/main.kt
@@ -1,7 +1,9 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.motorph.initKoin
 
 fun main() = application {
+    initKoin()
     Window(onCloseRequest = ::exitApplication) {
         Screen()
     }
