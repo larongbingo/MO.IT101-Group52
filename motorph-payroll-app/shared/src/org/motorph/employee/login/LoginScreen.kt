61e0da18059991088727358fd9118ad78ac18d5a
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-04-18 18:34:02 +0800

    feat: center login form

--- motorph-payroll-app/shared/src/org/motorph/employee/login/LoginScreen.kt
+++ motorph-payroll-app/shared/src/org/motorph/employee/login/LoginScreen.kt
@@ -1,12 +1,15 @@
 package org.motorph.employee.login
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
@@ -30,7 +33,11 @@ fun LoginScreen(
     onPasswordChanged: (String) -> Unit,
     login: (onSuccess: (Employee) -> Unit) -> Unit,
 ) {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        ) {
         Text(
             text = "Login",
         )
