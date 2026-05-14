0c8a9af5362664eb74439f3074575242991f6d39
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-05-14 16:56:41 +0800

    feat: add back button

--- motorph-payroll-swing-ui/src/org/motorph/employees/ViewEmployeeInfoPage.java
+++ motorph-payroll-swing-ui/src/org/motorph/employees/ViewEmployeeInfoPage.java
@@ -1,6 +1,6 @@
 package org.motorph.employees;
 
-import org.motorph.controls.EmployeeInfoPanel;
+import org.motorph.employees.controls.EmployeeInfoPanel;
 import org.motorph.listeners.AncestorListenerHandler;
 
 import javax.swing.*;
@@ -8,20 +8,24 @@ import java.awt.*;
 
 public class ViewEmployeeInfoPage extends JPanel {
     private final JPanel viewEmployeeInfoPanel;
+    private final JButton backButton;
     private final ViewEmployeeViewModel viewModel;
     public ViewEmployeeInfoPage(ViewEmployeeViewModel viewModel) {
         this.viewModel = viewModel;
         this.viewEmployeeInfoPanel = new JPanel();
+        this.backButton = new JButton("Back to Home");
 
         setVisible(true);
 
         addAncestorListener(new AncestorListenerHandler(this::addLayout));
+        backButton.addActionListener(e -> viewModel.GoBack());
     }
 
     private void addLayout() {
         Employee employee = viewModel.getEmployee();
         viewEmployeeInfoPanel.setLayout(new BorderLayout());
         viewEmployeeInfoPanel.add(new EmployeeInfoPanel(employee));
+        viewEmployeeInfoPanel.add(backButton, BorderLayout.SOUTH);
         add(viewEmployeeInfoPanel);
 
         viewEmployeeInfoPanel.revalidate();
--- motorph-payroll-swing-ui/src/org/motorph/employees/ViewEmployeeViewModel.java
+++ motorph-payroll-swing-ui/src/org/motorph/employees/ViewEmployeeViewModel.java
@@ -1,9 +1,15 @@
 package org.motorph.employees;
 
+import org.motorph.Routes;
+import org.motorph.Shell;
 import org.motorph.auth.CurrentEmployeeLoggedIn;
 
 public class ViewEmployeeViewModel {
     public Employee getEmployee() {
         return CurrentEmployeeLoggedIn.employee;
+    }
+
+    public void GoBack() {
+        Shell.navigate(Routes.APP);
     }
 }
