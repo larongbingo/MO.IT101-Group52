6885661fde0ccae897f55a8c9a068c8285ca0894
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-05-25 00:41:11 +0800

    refactor: extract application setup logic into separate `App` class

---
+++ motorph-payroll-swing-ui/src/org/motorph/App.java
@@ -0,0 +1,57 @@
+package org.motorph;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.motorph.auth.LoginForm;
+import org.motorph.auth.LoginViewModel;
+import org.motorph.data.LoadData;
+import org.motorph.employees.ManageEmployeeListForm;
+import org.motorph.employees.ManageEmployeeListViewModel;
+import org.motorph.employees.ViewEmployeeInfoForm;
+import org.motorph.employees.ViewEmployeeViewModel;
+import org.motorph.payroll.CalculatePayrollForm;
+import org.motorph.payroll.CalculatePayrollViewModel;
+import org.motorph.payroll.SelectTimesheetForm;
+import org.motorph.payroll.SelectTimesheetViewModel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+
+public class App {
+    private final JFrame frame;
+    private HashMap<String, JPanel> routing;
+    private Shell shell;
+    private final Injector injector;
+
+    public App() {
+        this(new JFrame(), Guice.createInjector(new AppModule()));
+    }
+
+    public App(JFrame frame, Injector injector) {
+        this.frame = frame;
+        this.injector = injector;
+        routing = new HashMap<String, JPanel>();
+        routing.put(Routes.APP, new AppForm(injector.getInstance(AppViewModel.class)).getRootPanel());
+        routing.put(Routes.LOGIN, new LoginForm(injector.getInstance(LoginViewModel.class)).getRootPanel());
+        routing.put(Routes.VIEW_EMPLOYEE, new ViewEmployeeInfoForm(injector.getInstance(ViewEmployeeViewModel.class)).getRootPanel());
+        routing.put(Routes.VIEW_PAYROLL, new SelectTimesheetForm(injector.getInstance(SelectTimesheetViewModel.class)).getRootPanel());
+        routing.put(Routes.VIEW_PAYROLL__CALCULATOR, new CalculatePayrollForm(injector.getInstance(CalculatePayrollViewModel.class)).getRootPanel());
+        routing.put(Routes.MANAGE_EMPLOYEES, new ManageEmployeeListForm(injector.getInstance(ManageEmployeeListViewModel.class)).getRootPanel());
+
+        shell = new Shell(routing);
+        Shell.Global = shell;
+
+        // Show the login page initially
+        shell.showPanel("login");
+    }
+
+    public void runApp() {
+        injector.getInstance(LoadData.class).initReposWithData();
+        frame.setMinimumSize(new Dimension(500, 300));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(shell);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
--- motorph-payroll-swing-ui/src/org/motorph/Main.java
+++ motorph-payroll-swing-ui/src/org/motorph/Main.java
@@ -13,28 +13,6 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        var injector = Guice.createInjector(new AppModule());
-        injector.getInstance(LoadData.class).initReposWithData();
-
-        var map = new HashMap<String, JPanel>();
-        map.put(Routes.APP, new AppForm(injector.getInstance(AppViewModel.class)).getRootPanel());
-        map.put(Routes.LOGIN, new LoginForm(injector.getInstance(LoginViewModel.class)).getRootPanel());
-        map.put(Routes.VIEW_EMPLOYEE, new ViewEmployeeInfoForm(injector.getInstance(ViewEmployeeViewModel.class)).getRootPanel());
-        map.put(Routes.VIEW_PAYROLL, new SelectTimesheetForm(injector.getInstance(SelectTimesheetViewModel.class)).getRootPanel());
-        map.put(Routes.VIEW_PAYROLL__CALCULATOR, new CalculatePayrollForm(injector.getInstance(CalculatePayrollViewModel.class)).getRootPanel());
-        map.put(Routes.MANAGE_EMPLOYEES, new ManageEmployeeListForm(injector.getInstance(ManageEmployeeListViewModel.class)).getRootPanel());
-
-        var shellPanel = new Shell(map);
-        Shell.Global = shellPanel;
-
-        // Show the login page initially
-        shellPanel.showPanel("login");
-
-        var frame = new JFrame("Motorph Payroll");
-        frame.setMinimumSize(new Dimension(500, 300));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(shellPanel);
-        frame.pack();
-        frame.setVisible(true);
+        new App();
     }
 }
